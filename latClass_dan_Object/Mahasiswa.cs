5ebc7afc666a7c0887d0963d2057e40fedfd2717
frand-kod <85376495+frand-kod@users.noreply.github.com>
2022-03-24 01:06:11 +0700

    Add files via upload
    file hasil mengikuti dari modul prakktikum 03

---
+++ latClass_dan_Object/Mahasiswa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latClass_dan_Object
+{
+   public class Mahasiswa
+    {
+        //properties
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        //method
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM : {0}", Nim);
+            Console.WriteLine("IPK : {0}", Ipk);
+            Console.WriteLine("Telah Melakukan Registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} Sedang Melakukan Proses KRS\n", Nama);
+
+        }
+    }
+}
